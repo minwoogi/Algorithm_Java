0a2f852d96b17705b2bf0ad85ebdcef281b40a7c
csc19 <csc19@DESKTOP-P8HHRGL>
2023-01-04 16:58:57 +0900

    chore : Add algorithm code

---
+++ Algorithm/src/baekjoon/Baekjoon_11659.java
@@ -0,0 +1,37 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.io.InputStreamReader;
+
+public class Baekjoon_11659 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int[] arr = new int[N + 1];
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 1; i <= N; i++) {
+			arr[i] = arr[i - 1] + Integer.parseInt(st.nextToken());
+		}
+
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			sb.append(arr[b] - arr[a - 1] + "\n");
+		}
+
+		System.out.println(sb);
+
+	}
+
+}
---
+++ Algorithm/src/baekjoon/Baekjoon_2108.java
@@ -0,0 +1,70 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.io.IOException;
+
+public class Baekjoon_2108 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		int sum = 0;
+
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+			sum += arr[i];
+		}
+
+		System.out.println((int) Math.round((double) sum / N)); // 1.»ê¼úÆò±Õ
+		Arrays.sort(arr);
+		System.out.println(arr[arr.length / 2 + 1]); // 2.Áß¾Ó°ª
+
+		int[] minus = new int[4002];
+		int[] plus = new int[4001];
+
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] < 0) {
+				minus[Math.abs(arr[i])]++;
+			} else {
+				plus[arr[i]]++;
+			}
+		}
+
+		ArrayList<Integer> list = new ArrayList<>();
+
+		int max = 0;
+		for (int i = 0; i < minus.length; i++) {
+			max = Math.max(minus[i], max);
+		}
+		for (int i = 0; i < plus.length; i++) {
+			max = Math.max(plus[i], max);
+		}
+
+		for (int i : arr) {
+			if (i < 0) {
+				if (minus[Math.abs(i)] == max && !(list.contains(i))) {
+					list.add(i);
+				}
+			} else {
+				if (plus[i] == max && !(list.contains(i))) {
+					list.add(i);
+				}
+			}
+		}
+
+		if (list.size() >= 2) { // 3.ÃÖºó°ª
+			System.out.println(list.get(1));
+		} else {
+			System.out.println(list.get(0));
+		}
+
+		System.out.println(arr[N - 1] - arr[0]); // 4.¹üÀ§
+	}
+
+}
