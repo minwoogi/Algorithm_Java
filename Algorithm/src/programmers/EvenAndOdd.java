121c71875cba6a68bd397fe6674b1aa6618bfd3a
minwoogi <csc1925@naver.com>
2023-01-31 14:25:20 +0900

    chore: Add algorithm code

---
+++ Algorithm/src/programmers/EvenAndOdd.java
@@ -0,0 +1,17 @@
+package programmers;
+
+public class EvenAndOdd {
+	
+	/**
+	 * Created by minwoogi on 2023/01/31 Â¦¼ö¿Í È¦¼ö
+	 ** https://school.programmers.co.kr/learn/courses/30/lessons/12937?language=java
+	 */
+
+	static class Solution {
+		public static String solution(int num) {
+			String answer = num % 2 == 0 ? "Even" : "Odd";
+			return answer;
+		}
+	}
+
+}
