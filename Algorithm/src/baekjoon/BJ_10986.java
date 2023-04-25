148dda3da547126afa4f5f11224668556abd5a89
Chu Minuk <csc1925@naver.com>
2023-04-25 14:51:27 +0900

    chore: Add algorithm code

---
+++ Algorithm/src/baekjoon/BJ_10986.java
@@ -0,0 +1,42 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ_10986 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int count = 0;
+		long[] prefixSum = new long[N + 1];
+		long[] cnt = new long[M];
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 1; i < N+1; i++) {
+			prefixSum[i] = (prefixSum[i - 1] + Integer.parseInt(st.nextToken())) % M;
+			if (prefixSum[i] == 0) {
+				count++;
+			}
+
+			cnt[(int) prefixSum[i]]++;
+		}
+
+		for (int i = 0; i < M; i++) {
+			if (cnt[i] > 1) {
+				count += (cnt[i] * (cnt[i] - 1)) / 2;
+			}
+
+		}
+
+		System.out.println(count);
+
+	}
+
+}
